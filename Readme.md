81c1244ec883ea2cb396b92bfc52e5fd0b968f53
Robert Gibb <rogi@universal-robots.com>
2019-05-11 09:23:54 -0400

    Added multiple bank support to memory bank class

--- Readme.md
+++ Readme.md
@@ -4,3 +4,7 @@
 A small project that I have been working on to familarize myself with TypeScript and my next step in writing emulators. This Readme will currently serve as a place for me to track my progress. Fascination reading material.
 
 * 5/9/19 - All JavaScript files have been converted over to TypeScript. I have created MemoryBank instances for most of the ram, but have realized that my scheme doesn't work well with the Imran Nazar's ramoffset. Current thoughts are to read eram size from cartridge header and have the bank instance for eram take in the number of 8k banks to create. Then when reading and writing, we can pass in the rambank number for read and writes.
+
+* 5/10/19 - Fixed most errors from linting. After looking over the MMU even more, I'm starting to think that I should be using the same scheme i use for handling ram banks with the roms banks as well. A: Better to use the same patter for similar operations and B: will probably help me debug. Now I need to come up with a good implementation.
+
+* 5/11/19 - Added multuple bank support to the MemoryBank class. You can now pass in the size of the memory you are trying to emulate, and it will create the appropriate number of banks based off the bank size. It will now allow you set the active bank as well, so writes can occur in the right bank.
--- src/models/MemoryBank.ts
+++ src/models/MemoryBank.ts
@@ -1,40 +1,65 @@
 import Address from './Address';
 
 export enum BankTypes {
-	VRAM,
-	WRAM,
-	SRAM,
-	ERAM,
-	ZRAM,
-	OAM
+    VRAM,
+    WRAM,
+    SRAM,
+    ERAM,
+    ZRAM,
+    OAM
 }
 
 const bankProps: Object = {
-	[BankTypes.VRAM]: { size: 8192, offset: 0x8000 },
-	[BankTypes.ERAM]: { size: 8192, offset: 0xA000 },
-	[BankTypes.WRAM]: { size: 8192, offset: 0xC000 },
-	[BankTypes.SRAM]: { size: 7679, offset: 0xE000 },
-	[BankTypes.ZRAM]: { size: 127, offset: 0xFF89 },
-	[BankTypes.OAM]: { size: 149, offset: 0xFE00 }
+    [BankTypes.VRAM]: { bankSize: 8192, offset: 0x8000 },
+    [BankTypes.ERAM]: { bankSize: 8192, offset: 0xA000 },
+    [BankTypes.WRAM]: { bankSize: 8192, offset: 0xC000 },
+    [BankTypes.SRAM]: { bankSize: 7679, offset: 0xE000 },
+    [BankTypes.ZRAM]: { bankSize: 127, offset: 0xFF89 },
+    [BankTypes.OAM]: { bankSize: 149, offset: 0xFE00 }
 
 };
 
 export default class MemoryBank {
 	
-	private _bank: ArrayBuffer;
+    private _bank: ArrayBuffer | ArrayBuffer[];
     private _offset: number;
-	
-	constructor(type: BankTypes) {
-        const { size, offset } = bankProps[type];
-		this._bank = new Uint8Array(size);
-        this._offset = offset;
-	}
+    private _numOfBanks: number = 0;
+    private _activeBank: number = 0;
 
-	getValue(address: Address): number {
-		return this._bank[address.getVal() - this._offset];
-	}
-	
-	setValue(address: Address, val: number): void {
-		this._bank[address.getVal()-this._offset] = val;
-	}
+    constructor(bankType: BankTypes);
+    constructor(bankType: BankTypes, size: number);
+    constructor(bankType: any, size?: any) {
+        const { bankSize, offset } = bankProps[bankType];
+
+        if (size) {
+            this._numOfBanks = Math.ceil(size / bankSize);
+            this._bank = new Array(this._numOfBanks);
+            
+            for (let x = 0; x < this._numOfBanks; x += 1) {
+                this._bank[x] = new Uint8Array(bankSize);
+            }
+        } else {
+            this._bank = new Uint8Array(bankSize);
+        }
+        
+        this._offset = offset;
+    }
+
+    getValue(address: Address): number {
+        const bank = this.getBank();
+        return bank[address.getVal() - this._offset];
+    }
+    
+    setValue(address: Address, val: number): void {
+        const bank = this.getBank();
+        bank[address.getVal()-this._offset] = val;
+    }
+
+    setActiveBank(actveBank: number): void {
+        this._activeBank = actveBank;
+    }
+
+    private getBank(): Uint8Array {
+        return this._numOfBanks ? this._bank[this._activeBank] : this._bank;
+    }
 }
