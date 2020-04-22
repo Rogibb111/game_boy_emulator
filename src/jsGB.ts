4692b2543ba00903c67cdd9ad279f800adddfef0
Rogibb111 <Rogibb111@users.noreply.github.com>
2020-04-21 22:49:01 -0400

    Add opcode and registers to instruction logic
    Co-authored-by: Robert Gibb <rogi@universal-robots.com>
    Pulled out opcode into seperate variable and am passing it as an argument to the instructions now, add registers as argument to instructions missing it (#23)

--- src/jsGB.ts
+++ src/jsGB.ts
@@ -23,7 +23,8 @@ class GameBoy {
     frame() {
         const fclk = Z80._clock.t + 70224;
         do {
-            Z80._map[MMU.rb(Z80._r.pc)]();
+            const opcode = MMU.rb(Z80._r.pc);
+            Z80._map[opcode](Z80._r, opcode);
             Z80._r.pc = Z80._r.pc.ADD(1).AND(65535);
             Z80._clock.m += Z80._r.m;
             Z80._clock.t += Z80._r.t;
