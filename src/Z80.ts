fbe408b26bcd07b873389e0ace51cc67e1df9472
Robert Gibb <Rogibb111@users.noreply.github.com>
2020-04-24 17:38:14 -0400

    Updated Z80 class member access rights, added 16bit instruction map, and function to execute 16 bit instructions from 8 bit instruction map (#27)

--- src/Z80.ts
+++ src/Z80.ts
@@ -32,31 +32,45 @@ class Z80 {
     ||    Internal      State     ||
     \*----------------------------*/
 
-    _clock = copy(_clock);
+    public _clock = copy(_clock);
 
-    _r: Registers = copy(_r);
+    public _r: Registers = copy(_r);
 
-    reset() {
+    public _map = {
+        0x83: Instructions.ADDr_e,
+        0xB8: Instructions.CPr_b,
+        0xF3: Instructions.DI,
+        0xFB: Instructions.EI,
+        0x3E: Instructions.LDAn,
+        0x00: Instructions.NOP,
+        0xE1: Instructions.POPHL,
+        0xC5: Instructions.PUSHBC,
+        0xD9: Instructions.RETI,
+        0x31: Instructions.LDSPnn,
+        0xAF: Instructions.XORA,
+        0x21: Instructions.LDHLnn,
+        0xCB: this._execute16BitInstruction
+    };
+
+    private _16BitInstructions = new Array(256);
+
+    constructor() {
+        for (let i = 0x40; i <= 0x7f; i++) {
+            this. _16BitInstructions[i] = Instructions.BITu3r8;
+        }
+    }
+
+    private _execute16BitInstruction(_r, instruction) {
+        const second_operand = instruction & 0xFF;
+        this. _16BitInstructions[second_operand](_r, instruction);
+    }
+    
+    public reset() {
         this._clock = copy(_clock);
         this._r = copy(_r);
         this._r.pc = new Address(0);
         this._r.sp = new Address(0);
     }
-
-    _map = {
-        [0x83]: Instructions.ADDr_e,
-        [0xB8]: Instructions.CPr_b,
-        [0xF3]: Instructions.DI,
-        [0xFB]: Instructions.EI,
-        [0x3E]: Instructions.LDAn,
-        [0x00]: Instructions.NOP,
-        [0xE1]: Instructions.POPHL,
-        [0xC5]: Instructions.PUSHBC,
-        [0xD9]: Instructions.RETI,
-        [0x31]: Instructions.LDSPnn,
-        [0xAF]: Instructions.XORA,
-        [0x21]: Instructions.LDHLnn
-    };
 };
 
 const instance: Z80 = new Z80();
