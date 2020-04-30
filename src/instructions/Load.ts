7c7c14b5d1f2f0ccd2bf4e8db9d4d6e5e96ef733
Rogibb111 <Rogibb111@users.noreply.github.com>
2020-04-29 20:41:33 -0400

    Adding LD r8, r8 instruction along with its byte map

--- src/instructions/Load.ts
+++ src/instructions/Load.ts
@@ -3,6 +3,7 @@ import Address from '../models/data_types/Address.js';
 import Registers from '../models/Registers.js';
 import Instruction from '../models/data_types/Instruction.js';
 import Opcode from '../models/data_types/Opcode.js';
+import Byte from '../models/data_sizes/Byte.js';
 
 const WORD_REGISTER_MAP = {
     0x01: ['b', 'c'],
@@ -10,9 +11,20 @@ const WORD_REGISTER_MAP = {
     0x21: ['h', 'l']
 };
 
+const BYTE_REGISTER_MAP = {
+    0x06: 'b',
+    0x0E: 'd',
+    0x16: 'h',
+    0x26: 'c',
+    0x2E: 'e',
+    0x36: 'l',
+    0x3E: 'a'
+};
+
 // Read a byte from absolute location into A (LD A, addr)
 export function LDAn(_r: Registers) {
     const addr: Address = new Address(MMU.rw(_r.pc));     // Get address from instr
+    
     _r.pc = this._r.pc.ADD(2);                            // Advance PC
     _r.a = MMU.rb(addr);                                  // Read from address
     _r.m = 4;                                             // 4 M-times taken
@@ -22,6 +34,7 @@ export function LDAn(_r: Registers) {
 export function LD_HLD_A(_r: Registers) {
     const addr: Address = new Address(_r.h, _r.l);
     const newAddr: Address = addr.ADD(-1);
+    
     _r.h = newAddr.getFirstByte();
     _r.l = newAddr.getLastByte();
     
@@ -33,9 +46,20 @@ export function LD_HLD_A(_r: Registers) {
 export function LD_RW_NW(_r: Registers, instruction: Instruction) {
     const opcode: Opcode = instruction.getFirstByte();
     const [upper, lower] = WORD_REGISTER_MAP[opcode.getVal()];
+    
     _r[lower] = MMU.rb(_r.pc);
     _r[upper] = MMU.rb(_r.pc.ADD(1));
     _r.pc = _r.pc.ADD(2);
     _r.m = 3;
     _r.t = 12;
+}
+
+export function LD_RB_NB(_r: Registers, instruction: Instruction) {
+    const opcode: Opcode = instruction.getFirstByte();
+    const reg = BYTE_REGISTER_MAP[opcode.getVal()];
+    const operand: Byte = instruction.getLastByte();
+
+    _r[reg] = operand;
+    _r.m = 2;
+    _r.t = 8;
 }
