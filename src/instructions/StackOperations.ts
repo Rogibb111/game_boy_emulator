5a692456fb11a3d277b40def93b16fe38357a2a7
Robert <rogibb111@gmail.com>
2020-05-22 23:38:21 -0400

    Starting to transition Push BC over to Push r16

--- src/instructions/StackOperations.ts
+++ src/instructions/StackOperations.ts
@@ -3,15 +3,18 @@ import MMU from '../MMU.js';
 import Registers from '../models/Registers.js';
 import { InstructionMetaData } from './InstructionMetaData.js';
 
-// Push registers B and C to the stack (PUSH BC)
-export const PUSHBC = {
+// Push r16 onto the stack one byte at a time
+export const PUSH_RW = {
     m: 4,
     t: 16,
-    action: ({ _r }) => {
+    action: function ({ _r }): void {
         _r.sp = _r.sp.ADD(-1);             // Drop through the stack
         MMU.wb(_r.sp, _r.b);               // Write B
         _r.sp = _r.sp.ADD(-1);            // Drop through the stack
         MMU.wb(_r.sp, _r.c);               // Write C
+    },
+    map: {
+
     },
     bytes: 1 
 } as InstructionMetaData;
@@ -37,3 +40,5 @@ export const LDSPnn = {
     },
     bytes: 3
 };
+
+
