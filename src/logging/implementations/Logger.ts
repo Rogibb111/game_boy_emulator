dcb72e7866a59f7e0265cf35a6bb34b83444ea36
rogibb111 <rob.gibb@livongo.com>
2020-07-15 23:03:21 -0600

    Adding file skeletons for implementations

---
+++ src/logging/implementations/Logger.ts
@@ -0,0 +1,12 @@
+import Aggregator from "./Aggregator.js";
+
+export default class Logger {
+    
+    setupLogging() {
+        if (this.hasOwnProperty('properties')) {
+        }
+        if (this.hasOwnProperty('functions')) {
+        
+        }
+    }
+}
