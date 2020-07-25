451f50ad3c0b898c4e949be047eaebd84263cad6
rogibb111 <rogibb111@gmail.com>
2020-07-24 22:55:04 -0600

    filling in aggregator and updating interfaces as necessary

--- src/logging/implementations/Aggregator.ts
+++ src/logging/implementations/Aggregator.ts
@@ -1,12 +1,39 @@
+import Display from '../interfaces/Display.js';
+
+interface MapVals {
+	args: any,
+	date: Date,
+	className: string,
+	funcName: string
+}
+
 class Aggregator {
-	logProperty(name: string, prop: string, value: any): void {
+
+	registeredDisplays: Array<Display> = [];
+	
+	unfinishedFunctions: Map<string, MapVals> = new Map();
+
+	registerDisplay(display: Display) {
+		this.registeredDisplays.push(display);	
 	}
 
-	logBeforeFunc(name: string, funcName: string, args: any): void {
+	logProperty(classId: number, className: string, prop: string, value: any): void {
+		for (const display in this.registeredDisplays) {
+			display.logProperties(className, prop, value);
+		}
+	}	
+
+	logBeforeFunc(classId: number, className: string, funcName: string, args: any): void {
+		// TODO: 	Need to figure out how store function starts so their information is retrievable in the right 
+		// 			order. Because multiple class instances could call the same function, or even one class
+		// 			instance could call the same function multiple times, im finding it hard to keep track of
+		// 			which start will belong to which end.
+		this.unfinishedFunctions.set(className, { args, date: new Date(), className, funcName });	
 	}
 
-	logAfterFunc(name: string, funcName: string, ret: any): void {
+	logAfterFunc(classId: number, className: string, funcName: string, ret: any): void {
 	}
+
 }
 const instance = new Aggregator();
 export default instance;
--- src/logging/interfaces/Display.ts
+++ src/logging/interfaces/Display.ts
@@ -1,8 +1,10 @@
-export interface Display {
-    loggingProfile: [{ 
+export default interface Display {
+    loggingProfile: [
+		{ 
             class: string,
             logTypes: string[]
-        }],
-    logProperites: (className: string, name: string, value: any) => null,
-    logFunctions: (className: string, name: string, start: Date, end: Date, args: any, ret: any) => null
+        }
+	],
+	logProperites: (classId: number, className: string, name: string, value: any) => null,
+    logFunctions: (classId: number, className: string, name: string, start: Date, end: Date, args: any, ret: any) => null
 }
