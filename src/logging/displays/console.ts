962fd2cce1c58b4f8892adc818074a87a855e39e
rogibb111 <rogibb111@gmail.com>
2020-07-27 21:26:36 -0600

    created display for a console log, and implemented the loggingProfile as well as the logProperties function. Had to work through some typescript syntax errors

---
+++ src/logging/displays/console.ts
@@ -0,0 +1,48 @@
+import Display, { LogTypes }  from '../interfaces/Display.js';
+
+const registers = {
+	'a': 0,
+	'b': 0,
+	'c': 0,
+	'd': 0,
+	'e': 0,
+	'f': 0,
+	'h': 0,
+	'pc': 0,
+	'sp': 0,
+	'm': 0,
+	't': 0,
+	'ime': 0
+};
+
+const registerNames: Array<string> = Object.keys(registers);
+
+function printRegisters() {
+	console.log('<------REGISTERS------>');
+	for (let key in registerNames) {
+		console.log(`${key}: ${registers[key]}`);
+	}
+}
+
+export default class Console implements Display {
+	loggingProfile = [
+		{
+			classType: 'Z80',
+			logTypes: [
+				LogTypes.properties,
+				LogTypes.functions
+			]
+		}
+	];	
+
+	logProperties(classId: number, className: string, name: string, value: any) {
+		if (className === 'Z80' && registerNames.includes(name)) {
+			registers[name] = value;
+			printRegisters();
+		}
+	}
+
+	logFunctions(classId: number, className: string, name: string, start: Date, end: Date, args: any, ret: any) {
+		
+	}
+}
